240d6f0a1d45fcd4f0dc1d5c2371960ec94b345d
Enrique Galvan <galvan_ie_castro@hotmail.com>
2022-11-01 15:29:56 -0600

    primer commit del nuevo intento de proyecto curso de programacion

---
+++ js/mokepon.js
@@ -0,0 +1,49 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador() {
+    //traer el valor de html de los inputs con respecto a los id´s
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+
+    // span para campos dinámicos
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
+
+    // .checked para verificar si el valor está seleccionado
+    if(inputHipodoge.checked){
+        // innerHTML para mainupar y mostrar resultados en el DOM
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if(inputCapipepo.checked){
+        spanMascotaJugador.innerHTML = 'Capipepo'
+    } else if(inputRatigueya.checked){
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } else{
+        alert("Debes seleccionar una mascota")
+    }
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo(){
+    // misma lógica de funcion de arriba (seleccionarMascotaJugador) + aleatorio
+    let mascotaAleatorio = aleatorio(1, 3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+    if (mascotaAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (mascotaAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else{
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// evento para que script corra desde el inicio
+window.addEventListener('load', iniciarJuego)
